7623966feff12a265de84d150ad59c9b063bae12
pierreboissinot <pierre.boissinot@lephare.com>
2021-01-07 15:48:15 +0100

    feat(i18n): add fr locale

---
+++ src/lang/fr/days.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    0 => 'Lundi',
+    1 => 'Lundi',
+    2 => 'Mardi',
+    3 => 'Mercredi',
+    4 => 'Jeudi',
+    5 => 'Vendredi',
+    6 => 'Samedi',
+    7 => 'Dimanche',
+];
