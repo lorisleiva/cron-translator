621fcf8171764076285aca0c12b68882168e394e
sishok02 <sishokkerman@gmail.com>
2022-11-05 17:59:04 +0200

    Test - Final version of Romanian language test and some fixes

--- src/lang/ro/fields.php
+++ src/lang/ro/fields.php
@@ -3,13 +3,13 @@
 return [
     'minutes' => [
         'every' => 'în fiecare minut',
-        'increment' => 'în ficeare :increment minute',
+        'increment' => 'în fiecare :increment minute',
         'times_per_increment' => ':times fiecare :increment minute',
         'multiple' => ':times pe oră',
     ],
     'hours' => [
         'every' => 'în fiecare oră',
-        'once_an_hour' => 'odată pe oră',
+        'once_an_hour' => 'o singură dată pe oră',
         'increment' => 'în fiecare :increment ore',
         'multiple_per_increment' => ':count ore din :increment',
         'times_per_increment' => ':times fiecare :increment ore',
@@ -31,7 +31,7 @@ return [
         'every_on_day' => 'pe data de :day a fiecărei luni',
         'increment' => 'în fiecare :increment luni',
         'multiple_per_increment' => ':count luni din :increment',
-        'multiple_per_year' => ':count lună a anului',
+        'multiple_per_year' => ':count luni pe an',
         'once_on_month' => 'în luna :month',
         'once_on_day' => 'în luna :month pe data de :day',
     ],
@@ -39,8 +39,8 @@ return [
         'every' => 'în fiecare :weekday',
         'increment' => 'în fiecare :increment zile din săptămână',
         'multiple_per_increment' => ':count zile ale săptămânii din :increment',
-        'multiple_days_a_week' => ':count zile ale sătpămânii',
-        'once_on_day' => 'în zilele de :days',
+        'multiple_days_a_week' => ':count zile ale săptămânii',
+        'once_on_day' => 'în zilele de :day',
     ],
     'years' => [
         'every' => 'în fiecare an',
