c16efd593b4ae884ed75b16361d0bc24dc06a137
Ondřej Kašpar <ondrej.kaspar@digital.cz>
2025-05-15 14:17:03 +0200

    cs lang

---
+++ src/lang/cs/days.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    1 => 'Pondělí',
+    2 => 'Úterý',
+    3 => 'Středa',
+    4 => 'Čtvrtek',
+    5 => 'Pátek',
+    6 => 'Sobota',
+    7 => 'Neděle',
+];
---
+++ src/lang/cs/months.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    1 => 'leden',
+    2 => 'únor',
+    3 => 'březen',
+    4 => 'duben',
+    5 => 'květen',
+    6 => 'červen',
+    7 => 'červenec',
+    8 => 'srpen',
+    9 => 'září',
+    10 => 'říjen',
+    11 => 'listopad',
+    12 => 'prosinec',
+];
